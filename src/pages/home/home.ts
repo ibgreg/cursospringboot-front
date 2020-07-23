583bc9229951febdec9292a69c40f340d7da8c7f
Ítalo Gregório <italo-07@hotmail.com>
2020-07-23 19:23:02 -0300

    Binding dos campos no formulário de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @IonicPage()
 @Component({
@@ -8,11 +9,18 @@ import { NavController, IonicPage, MenuController } from 'ionic-angular';
 })
 export class HomePage {
 
+  cred : CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
+
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
   login() {
+    console.log(this.cred);
+    
     this.navCtrl.setRoot('CategoriasPage');
   }
 
